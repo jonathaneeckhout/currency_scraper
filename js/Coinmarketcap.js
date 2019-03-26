57b8e6c45802561b13c7fb30e073c6e860191b80
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2019-03-27 00:46:17 +0100

    scraping working for top 10 currecnies

--- js/Coinmarketcap.js
+++ js/Coinmarketcap.js
@@ -25,7 +25,21 @@ class Coinmarketcap {
             }.bind(this));
         }.bind(this)).then(function (result) {
             try {
-                return JSON.parse(result);
+                var currencies = JSON.parse(result);
+                var retval = [];
+
+                for (var i = 0; i  < currencies.length; i++) {
+                   
+                    var cur = currencies[i];
+                    cur.id = cur.id.replace(/-/g, "_");
+                    if (this.scraper.currencies.includes(cur.id)) {
+                        if(cur.max_supply === null) {
+                            cur.max_supply = '0';
+                        }
+                        retval.push(this.scraper.setCurrencyValue(cur.id, cur.last_updated, cur.price_usd, cur.price_btc, cur["24h_volume_usd"], cur.market_cap_usd, cur.available_supply, cur.total_supply, cur.max_supply))
+                    }
+                }
+                return retval;
             } catch(e) {
                return null;
             }
--- js/Scraper.js
+++ js/Scraper.js
@@ -4,8 +4,23 @@ class Scraper {
     constructor(interval) {
         this.https = require('https');
         this.fs = require('fs');
+        this.mysql = require('mysql');
+        this.connection = null;
+        this.connectionConfig = null;
         this.interval = interval;
         this.sites = [];
+        this.currencies = [
+            "bitcoin",
+            "ethereum",
+            "ripple",
+            "litecoin",
+            "eos",
+            "bitcoin_cash",
+            "binance_coin",
+            "tether",
+            "stellar",
+            "cardano"
+        ];
         this.timer = undefined;
     };
 
@@ -16,8 +31,11 @@ class Scraper {
             promises.push(this.sites[site].scrape());
         }
         Promise.all(promises).then(function (data) {
-            console.log("Scraped all sites. Merging and storing data");
-            console.log(data);
+            this.connection = this.mysql.createConnection(this.connectionConfig);
+            for (var i = 0; i < data.length; i++) {
+                this.pushDataToMysql(data[i]);
+            }
+            this.connection.end();
         }.bind(this));
     };
 
@@ -31,8 +49,45 @@ class Scraper {
     };
 
     init() {
+        var rawMysqlConfig = this.fs.readFileSync('mysql_config.json');
+        this.connectionConfig = JSON.parse(rawMysqlConfig);
+
         this.sites.push(new Coinmarketcap(this));
     };
+
+    pushDataToMysql(data) {
+        for (var i = 0; i < data.length; i++) {
+            var cur = data[i];
+            let sql = 'INSERT INTO ' + cur.id + '(price, price_btc,volume_24h,market_cap,available_supply,total_supply,max_supply) VALUES(' 
+            + cur.price +','
+            + cur.price_btc +','
+            + cur.volume_24h +','
+            + cur.market_cap +','
+            + cur.available_supply +','
+            + cur.total_supply +','
+            + cur.max_supply +
+            ')';
+
+            // execute the insert statment
+            this.connection.query(sql, function (error, results, fields) {
+                if (error) console.log(error);
+            });
+        }
+    }
+
+    setCurrencyValue(id, time, price, price_btc, volume_24h, market_cap, available_supply, total_supply, max_supply) {
+        return {
+            id: id,
+            time: time,
+            price: price,
+            price_btc: price_btc,
+            volume_24h: volume_24h,
+            market_cap: market_cap,
+            available_supply: available_supply,
+            total_supply: total_supply,
+            max_supply: max_supply
+        }
+    }
 
 };
 
