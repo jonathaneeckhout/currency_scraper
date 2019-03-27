7417e309bf1a9a1a4ca7b8c21625b3a9c9f82488
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2019-03-27 01:01:40 +0100

    use normal interval again

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var Scraper = require("./js/Scraper")
 
-//var scraper = new Scraper(10 * 60 * 1000); // every 10 minutes
-var scraper = new Scraper(10 * 1000); // every 10 minutes
+var scraper = new Scraper(10 * 60 * 1000); // every 10 minutes
 
 scraper.init();
 
